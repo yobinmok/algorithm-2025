22506f57e4e3ab7a3be6f1a77bbc2d872b8faada
Mok Yobin <dyqls1214@naver.com>
2025-02-19 01:09:37 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 188 ms, Memory: 20372 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -10,24 +9,26 @@ public class Main {
 		
 		// 높이와 높이별 연결여부에 따라 처리
 		// 이전 높이가 자신보다 낮으면 그 층보다 높은층들은 연결여부 false
-		int ans = 0, prev = 0, max = 0;
+		int ans = 0, prev = 0;
 		boolean[] visited = new boolean[500001];
 		visited[0] = true;
 		
+		Stack<Integer> stk = new Stack<>();
 		for(int i = 0; i<n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken()); // 무조건 오름차순으로 입력됨
 			int y = Integer.parseInt(st.nextToken()); // 연결여부 확인, 같은 건물인지 확인
 			
-			max = Math.max(y, max);
-			
 			if(prev > y) {
-				Arrays.fill(visited, y+1, max+1, false);
+				while(stk.size() > 0 && stk.peek() > y) {
+					visited[stk.pop()] = false;
+				}
 			}
 			
-			if(!visited[y]) { // 해당 높이 건물을 세지 않았다면
+			if(!visited[y]) {
 				ans += 1;
 				visited[y] = true;
+				stk.add(y);
 			}
 			
 			prev = y;
