0fb1914cc7fc86237ea46d18bd505b52268b5532
Mok Yobin <dyqls1214@naver.com>
2025-04-01 17:55:53 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 140 ms, Memory: 14920 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 public class Main{
 	
@@ -11,18 +12,18 @@ public class Main{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
+		int[][] dp = new int[10001][4]; 
+		
+		// dp 초기화
+		for(int i = 1; i<=3; i++) {
+			for(int j = 1; j<=i; j++) {
+				dp[i][j] = 1;
+			}
+		}	
 		
 		for(int t = 0; t<T; t++) {
 			int n = Integer.parseInt(br.readLine()); // < 10,000
-			int[][] dp = new int[10001][4]; 
-			
-			// dp 초기화
-			for(int i = 1; i<=3; i++) {
-				for(int j = 1; j<=i; j++) {
-					dp[i][j] = 1;
-				}
-			}			
-			
+				
 			for(int i = 4; i<=n; i++) {
 				dp[i][1] = 1;
 				dp[i][2] = dp[i-2][1] + dp[i-2][2];
